b147e239ee2109083e7cea630ffcdb4392105e4c
Shubh4149 <49918384+Shubh4149@users.noreply.github.com>
2026-06-19 19:43:16 +0530

    Routing Changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <CoinContextProvider>
-        <App />
-      </CoinContextProvider>
-    </BrowserRouter>
+    <CoinContextProvider>
+      <App />
+    </CoinContextProvider>
   </StrictMode>,
 );
